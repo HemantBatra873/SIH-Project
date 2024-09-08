489d9e1ffd17280442c4b0cef6823570cad5dcfd
HemantBatra873 <domspidy050204@gmail.com>
2024-09-08 13:28:01 +0530

    Added header and fonts

--- SIH_Client/src/App.jsx
+++ SIH_Client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import About from './pages/About'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
--- SIH_Client/src/components/Header.jsx
+++ SIH_Client/src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="flex justify-between items-center mx-auto p-1 md:p-4 max-w-7xl">
+        <Link to='/'>
+          <h1 className='font-head text-2xl sm:text-3xl lg:text-5xl flex flex-wrap font-semibold'>Bracket Brigades</h1>
+        </Link>
+      <ul className='flex justify-center sm:gap-8 items-center mr-8 text-xl'>
+        <Link to='/about'><li className='hidden sm:inline'>About</li></Link>
+        <Link to='/login'><li>Login</li></Link>
+      </ul>
+      </div>
+    </header>
   )
 }
 
