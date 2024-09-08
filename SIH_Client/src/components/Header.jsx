7ad669eb933a496528bd9f1a780a56da3968d8fa
Harsh Gupta <creativezone171971.cz@gmail.com>
2024-09-08 16:32:00 +0530

    Update Header.jsx
    Just Update the title to recheck!

--- SIH_Client/src/components/Header.jsx
+++ SIH_Client/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header>
       <div className="flex justify-between items-center mx-auto p-1 md:p-4 max-w-7xl">
         <Link to='/'>
-          <h1 className='font-head text-2xl sm:text-3xl lg:text-5xl flex flex-wrap font-semibold'>Bracket Brigades</h1>
+          <h1 className='font-head text-2xl sm:text-3xl lg:text-5xl flex flex-wrap font-semibold'>Science Museum</h1>
         </Link>
       <ul className='flex justify-center sm:gap-8 items-center mr-8 text-xl'>
         <Link to='/about'><li className='hidden sm:inline'>About</li></Link>
