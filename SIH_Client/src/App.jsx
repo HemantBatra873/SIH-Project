0a3850c494cb5f34afc13a188195b803e386db60
HemantBatra873 <domspidy050204@gmail.com>
2024-09-10 10:27:17 +0530

    removed login page

--- SIH_Client/src/App.jsx
+++ SIH_Client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Home from './pages/Home'
-import Login from './pages/Login'
 import About from './pages/About'
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
---
+++ SIH_Client/src/components/ServerChatBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ServerChatBox = () => {
+  return (
+    <div>
+      ServerChatBox
+    </div>
+  )
+}
+
+export default ServerChatBox
